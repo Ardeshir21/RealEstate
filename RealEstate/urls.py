ac45fd3d8c66d4d39b158834d71d4f51b425a7aa
Ardeshir <examples21@gmail.com>
2020-06-15 12:25:24 +0300

    Change Admin URL

--- RealEstate/urls.py
+++ RealEstate/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     # This is for Blog Editor in blogApp.models.py
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('robots.txt', include('robots.urls')),
-    path('admin/', admin.site.urls),
+    path('management-login-page/', admin.site.urls),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
