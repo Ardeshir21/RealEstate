912232e86bfe7284a7df6f230066178f6feb7aa5
Ardeshir <examples21@gmail.com>
2025-05-21 13:23:20 +1000

    class based bots

--- apps/telegramApp/urls.py
+++ apps/telegramApp/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'telegramApp'
 
 urlpatterns = [
-    path('', views.TelegramDictionaryBotView.as_view(), name='telegram_bot'),
+    path('', views.TelegramWebhookView.as_view(), name='telegram_bot'),
 ]
